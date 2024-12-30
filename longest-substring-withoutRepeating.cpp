b84eae3b394ae1bbe582307c018360437f344dc1
far-hana5 <farhanahussain5730532@gmail.com>
2024-12-30 19:42:11 +0600

    problrm-2

---
+++ longest-substring-withoutRepeating.cpp
@@ -0,0 +1,95 @@
+
+/*
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(string str) {
+
+  if(str.size()==0)
+      return 0;
+  int maxans = INT_MIN;
+  unordered_set < int > set;
+  int l = 0,r=0;
+  while(r<str.size()) // outer loop for traversing the string
+  {
+    if (set.find(str[r]) != set.end()) //if duplicate element is found
+    {
+      while (l < r && set.find(str[r]) != set.end()) {
+        set.erase(str[l]);
+        l++;
+      }
+    }
+    set.insert(str[r]);
+    maxans = max(maxans, r - l + 1);r++;
+  }
+  return maxans;
+}
+
+int main() {
+  string str = "takeUforward";
+  cout << "The length of the longest substring without repeating characters is " << 
+  solve(str);
+  return 0;
+}
+
+
+//  brute force
+
+/*#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(string str) {
+
+  if(str.size()==0)
+      return 0;
+  int maxans = INT_MIN;
+  for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
+  {
+    unordered_set < int > set;
+    for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
+    {
+      if (set.find(str[j]) != set.end()) // if element if found so mark it as ans and break from the loop
+      {
+        maxans = max(maxans, j - i);
+        break;
+      }
+      set.insert(str[j]);
+    }
+  }
+  return maxans;
+}
+
+int main() {
+  string str = "takeUforward";
+  cout << "The length of the longest substring without repeating characters is " << 
+  solve(str);
+  return 0;
+}*/
